a792182587830715239e3e34601b3dd2230e93bc
siwonkim <ktldnjs0811@naver.com>
2023-04-26 19:51:19 +0900

    Initial commit

---
+++ day2_assignment/src/main/java/com/example/day2_assignment/Day2AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.day2_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day2AssignmentApplication.class, args);
+    }
+
+}
---
+++ day2_assignment/src/test/java/com/example/day2_assignment/Day2AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day2_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
